5d9c461eac617be738d64a5d112b7b7a83bd6dbf
SamGeek <sam.dagba@gmail.com>
2019-08-06 10:43:50 +0100

    update on logic

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,27 +4,17 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+var moment = require('moment');
 
 module.exports = {
 
 
   computeData: async function(req,res){
 
-    var birthDate = req.allParams().birthDate
-
-    //determine the age
-    var today = new Date();
-    var birthDate = new Date(req.allParams().birthDate);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-
     return res.ok({
       name:req.allParams().name,
       birthDate:req.allParams().birthDate,
-      age:age,
+      age:moment(req.allParams().birthDate, "DDMMYYYY").fromNow().split(" ")[0],
     })
 
   },
